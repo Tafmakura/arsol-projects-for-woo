f91f027fd910dbdac1e9cfbf60239471f6acfdaa
Taf Makura <tafmakura@gmail.com>
2025-05-14 11:53:27 +0200

    Update class-admin-woo-orders.php

--- includes/classes/class-admin-woo-orders.php
+++ includes/classes/class-admin-woo-orders.php
@@ -269,7 +269,10 @@ class AdminOrders {
 
     public function save_project_field($order_id) {
         if (isset($_POST['arsol_project'])) {
-        
+            $order = wc_get_order($order_id);
+            $project_id = sanitize_text_field($_POST['arsol_project']);
+            
+           
             $order->update_meta_data(self::PROJECT_META_KEY, (int)$project_id);
             
             $order->save();
