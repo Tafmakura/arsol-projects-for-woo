bde6c77d46b86e75535241e3c9e34d0a9c00c654
Taf Makura <tafmakura@gmail.com>
2025-05-14 13:33:35 +0200

    Update class-admin-woo-orders.php

--- includes/classes/class-admin-woo-orders.php
+++ includes/classes/class-admin-woo-orders.php
@@ -213,7 +213,6 @@ class AdminOrders {
             } else {
                 // Parent order - show selector
                 $selected_project = $order->get_meta(self::PROJECT_META_KEY);
-                echo 'red>>>>>>'.$selected_project;
                 $projects = $this->get_projects();
                 ?>
                 <p class="form-field form-field-wide">
