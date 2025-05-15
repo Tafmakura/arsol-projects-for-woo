7eac4e35af4eb2e637d0ef9d4931fef7123bd99c
Taf Makura <tafmakura@gmail.com>
2025-05-15 21:33:08 +0200

    update

--- includes/classes/class-endpoints.php
+++ includes/classes/class-endpoints.php
@@ -26,8 +26,79 @@ class Endpoints {
      * Constructor
      */
     public function __construct() {
-        // Constructor is empty after removing all method calls
+        // Register endpoints
+        add_action('init', array($this, 'register_endpoints'));
+        
+        // Add Projects to account menu
+        add_filter('woocommerce_account_menu_items', array($this, 'add_projects_menu_item'));
+        
+        // Handle endpoint content
+        add_action('woocommerce_account_projects_endpoint', array($this, 'projects_endpoint_content'));
     }
     
+    /**
+     * Register custom rewrite endpoints
+     *
+     * @return void
+     */
+    public function register_endpoints() {
+        add_rewrite_endpoint('projects', EP_ROOT | EP_PAGES);
+    }
+    
+    /**
+     * Add Projects to WooCommerce account menu
+     *
+     * @param array $items Menu items
+     * @return array Modified menu items
+     */
+    public function add_projects_menu_item($items) {
+        $logout = $items['customer-logout'];
+        unset($items['customer-logout']);
+        $items['projects'] = __('Projects', 'arsol-pfw');
+        $items['customer-logout'] = $logout;
+        return $items;
+    }
+    
+    /**
+     * Display content for the projects endpoint
+     *
+     * @return void
+     */
+    public function projects_endpoint_content() {
+        // Get current user ID
+        $user_id = get_current_user_id();
+        
+        // Set up pagination
+        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+        $posts_per_page = 10; // Number of projects per page
+        
+        // Query user projects
+        $args = array(
+            'post_type'      => 'project', // Adjust if your CPT has a different name
+            'posts_per_page' => $posts_per_page,
+            'paged'          => $paged,
+            'meta_query'     => array(
+                array(
+                    'key'     => '_project_user_id', // Adjust based on how you store user association
+                    'value'   => $user_id,
+                    'compare' => '='
+                )
+            )
+        );
+        
+        $projects_query = new \WP_Query($args);
+        $has_projects = $projects_query->have_posts();
+        
+        // Pagination data
+        $total_pages = $projects_query->max_num_pages;
+        $current_page = max(1, $paged);
+        
+        // Button class for WooCommerce 3.5+
+        $wp_button_class = function_exists('wc_wp_theme_get_element_class_name') ? 
+            ' ' . wc_wp_theme_get_element_class_name('button') : '';
+        
+        // Include the template
+        include ARSOL_PFW_PATH . 'includes/ui/templates/frontend/projects.php';
+    }
 }
 
--- includes/ui/templates/frontend/projects.php
+++ includes/ui/templates/frontend/projects.php
@@ -14,64 +14,10 @@ do_action('arsol_projects_before_user_projects', $has_projects);
 ?>
 
 <div class="woocommerce">
-    <?php if ($has_projects) : ?>
-        <table class="woocommerce-orders-table woocommerce-MyAccount-orders shop_table shop_table_responsive my_account_orders account-orders-table user-projects-table">
-            <thead>
-                <tr>
-                    <th scope="col" class="woocommerce-orders-table__header woocommerce-orders-table__header-project-name"><span class="nobr"><?php esc_html_e('Name', 'arsol-projects-for-woo'); ?></span></th>
-                    <th scope="col" class="woocommerce-orders-table__header woocommerce-orders-table__header-project-actions"><span class="nobr"></span></th>
-                </tr>
-            </thead>
-
-            <tbody>
-                <?php
-                while ($projects_query->have_posts()) {
-                    $projects_query->the_post();
-                    $project_id = get_the_ID();
-                    ?>
-                    <tr class="woocommerce-orders-table__row project">
-                        <th class="woocommerce-orders-table__cell woocommerce-orders-table__cell-project-name" data-title="<?php esc_attr_e('Name', 'arsol-projects-for-woo'); ?>" scope="row">
-                            <a href="<?php echo esc_url(get_permalink($project_id)); ?>" aria-label="<?php echo esc_attr(sprintf(__('View project %s', 'arsol-projects-for-woo'), get_the_title())); ?>">
-                                <?php echo esc_html(get_the_title()); ?>
-                            </a>
-                        </th>
-                        <td class="woocommerce-orders-table__cell woocommerce-orders-table__cell-project-actions" data-title="">
-                            <a href="<?php echo esc_url(get_permalink($project_id)); ?>" class="woocommerce-button button view<?php echo esc_attr($wp_button_class); ?>"><?php esc_html_e('View', 'arsol-projects-for-woo'); ?></a>
-                        </td>
-                    </tr>
-                    <?php
-                }
-                wp_reset_postdata();
-                ?>
-            </tbody>
-        </table>
-
-        <?php do_action('arsol_projects_before_user_projects_pagination'); ?>
-
-        <?php if ($total_pages > 1) : ?>
-            <div class="woocommerce-pagination woocommerce-pagination--without-numbers woocommerce-Pagination">
-                <?php 
-                // Get current URL and preserve existing query args
-                $current_url = remove_query_arg('paged');
-                ?>
-                
-                <?php if (1 !== $current_page) : ?>
-                    <a class="woocommerce-button woocommerce-button--previous woocommerce-Button woocommerce-Button--previous button<?php echo esc_attr($wp_button_class); ?>" href="<?php echo esc_url(add_query_arg('paged', $current_page - 1, $current_url)); ?>"><?php esc_html_e('Previous', 'arsol-projects-for-woo'); ?></a>
-                <?php endif; ?>
-
-                <?php if ($total_pages !== $current_page) : ?>
-                    <a class="woocommerce-button woocommerce-button--next woocommerce-Button woocommerce-Button--next button<?php echo esc_attr($wp_button_class); ?>" href="<?php echo esc_url(add_query_arg('paged', $current_page + 1, $current_url)); ?>"><?php esc_html_e('Next', 'arsol-projects-for-woo'); ?></a>
-                <?php endif; ?>
-            </div>
-        <?php endif; ?>
-
-    <?php else : ?>
-
-        <div class="woocommerce-message woocommerce-message--info woocommerce-Message woocommerce-Message--info woocommerce-info">
-            <?php esc_html_e('No projects found.', 'arsol-projects-for-woo'); ?>
-        </div>
-
-    <?php endif; ?>
+    <?php 
+    // Use the existing shortcode to display user projects
+    echo do_shortcode('[user_projects per_page="' . esc_attr($posts_per_page) . '" paged="' . esc_attr($paged) . '"]'); 
+    ?>
 </div>
 
 <?php do_action('arsol_projects_after_user_projects', $has_projects); ?>
