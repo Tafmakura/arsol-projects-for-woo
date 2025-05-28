157d10d3da32cc15280739d4da5db343b443a2ad
Taf Makura <tafmakura@gmail.com>
2025-05-28 16:07:46 +0200

    Update section-project-overview-empty.php

--- includes/ui/components/frontend/section-project-overview-empty.php
+++ includes/ui/components/frontend/section-project-overview-empty.php
@@ -13,7 +13,7 @@ defined('ABSPATH') || exit;
 do_action('arsol_projects_before_empty_state', $project_id);
 ?>
 <div class="project-empty-state">
-    <h3><?php esc_html_e('Project Overview', 'arsol-projects-for-woo'); ?></h3>
-    <p><?php esc_html_e('This project is currently being set up. Check back soon for more details.', 'arsol-projects-for-woo'); ?></p>
+    <h3><?php esc_html_e('No Project Overview at the moment', 'arsol-projects-for-woo'); ?></h3>
+    <p><?php esc_html_e('This project has no overview information, this is ususally the case if it is still being set up, or there is no relevent information to share. Check back soon for more details, contact our support team for more information about this project.', 'arsol-projects-for-woo'); ?></p>
 </div>
 <?php do_action('arsol_projects_after_empty_state', $project_id); ?>
