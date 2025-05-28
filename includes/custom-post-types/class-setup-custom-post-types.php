0826025100f4b746d5b429db084e0be064563c7c
Taf Makura <tafmakura@gmail.com>
2025-05-28 23:06:05 +0200

    Update class-setup-custom-post-types.php

--- includes/custom-post-types/class-setup-custom-post-types.php
+++ includes/custom-post-types/class-setup-custom-post-types.php
@@ -14,11 +14,10 @@ class Setup {
 
     private function require_files() {
         // Custom Post Types
-        require_once ARSOL_PROJECTS_PLUGIN_DIR . 'includes/custom-post-types/project/class-project-cpt-setup.php';
+        require_once ARSOL_PROJECTS_PLUGIN_DIR . 'includes/custom-post-types/project/class-project-cpt-admin-setup.php';
     }
 
     private function instantiate_classes() {
-        new \Arsol_Projects_For_Woo\Custom_Post_Types\ProjectPost\Setup();
-
+        new \Arsol_Projects_For_Woo\Custom_Post_Types\ProjectPost\Admin\Admin_Setup();
     }
 }
