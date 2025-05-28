673389ff49d4fefe77ac1329c6165dccd40f16c8
Taf Makura <tafmakura@gmail.com>
2025-05-29 00:53:03 +0200

    Update class-admin-setup.php

--- includes/classes/class-admin-setup.php
+++ includes/classes/class-admin-setup.php
@@ -14,39 +14,4 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-class Setup {
-    /**
-     * Constructor
-     */
-    public function __construct() {
-        // Add menu item
-        add_action('admin_menu', array($this, 'add_admin_menu'));
-    }
-
-    /**
-     * Add menu item to WordPress admin menu
-     */
-    public function add_admin_menu() {
-        add_menu_page(
-            __('Arsol Projects For Woo', 'arsol-pfw'),
-            __('Arsol Projects', 'arsol-pfw'),
-            'manage_options',
-            'arsol-projects',
-            array($this, 'render_settings_page'),
-            'dashicons-portfolio',
-            56
-        );
-    }
-
-    /**
-     * Render the settings page
-     */
-    public function render_settings_page() {
-        if (!current_user_can('manage_options')) {
-            return;
-        }
-
-        // Include the template
-        include ARSOL_PROJECTS_PLUGIN_DIR . 'includes/ui/templates/admin/page-admin-settings-general.php';
-    }
-}
+// Removed the Setup class and its menu logic as settings are now a submenu under the CPT.
