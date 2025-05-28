e868d95dd1844439741b8b3d128650e264d1294b
Taf Makura <tafmakura@gmail.com>
2025-05-28 16:27:58 +0200

    Update section-project-overview-empty.php

--- includes/ui/components/frontend/section-project-overview-empty.php
+++ includes/ui/components/frontend/section-project-overview-empty.php
@@ -13,13 +13,6 @@ defined('ABSPATH') || exit;
 do_action('arsol_projects_before_empty_state', $project_id);
 ?>
 <div class="project-empty-state">
-    <div class="empty-state-icon">
-        <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#CCCCCC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M12 16V12" stroke="#CCCCCC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M12 8H12.01" stroke="#CCCCCC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-    </div>
     <h3><?php esc_html_e('No Project Overview at the moment', 'arsol-projects-for-woo'); ?></h3>
     <p><?php esc_html_e('This project has no overview information. This is usually the case if it is still being set up, or there is no relevant information to share. Check back soon for more details.', 'arsol-projects-for-woo'); ?></p>
     
