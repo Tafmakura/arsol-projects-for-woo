21d29417bd68f3d85d886eff64a1bbcf2896ff89
Taf Makura <tafmakura@gmail.com>
2025-05-28 19:53:30 +0200

    Update page-project-subscriptions.php

--- includes/ui/templates/frontend/page-project-subscriptions.php
+++ includes/ui/templates/frontend/page-project-subscriptions.php
@@ -22,7 +22,7 @@ do_action('arsol_projects_before_project_subscriptions', $project_id);
         <p><?php printf(
             /* translators: %s: subscriptions */
             esc_html__('This table gives you a live view of all %s. Monitor status, payment schedules, and manage your recurring transactions in one place.', 'arsol-projects-for-woo'),
-            '<span class="woocommerce-subscriptions">' . esc_html__('subscriptions', 'woocommerce-subscriptions') . '</span>'
+            '<span>' . esc_html__('subscriptions', 'woocommerce-subscriptions') . '</span>'
         ); ?></p>
         <?php 
         // Use the project_subscriptions shortcode to render the table
