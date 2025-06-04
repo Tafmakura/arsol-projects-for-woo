585bdef5e05402b6439ad0a36a01ae03bee48995
Taf Makura <tafmakura@gmail.com>
2025-06-05 00:23:09 +0200

    Update section-projects-create-or-request.php

--- includes/ui/components/frontend/section-projects-create-or-request.php
+++ includes/ui/components/frontend/section-projects-create-or-request.php
@@ -7,23 +7,22 @@ $user_id = get_current_user_id();
 $can_create = \Arsol_Projects_For_Woo\Admin\Admin_Capabilities::can_create_projects($user_id);
 $can_request = \Arsol_Projects_For_Woo\Admin\Admin_Capabilities::can_create_project_requests($user_id);
 
+$button_url = '';
+$button_label = '';
 if ($can_create) {
     $button_url = wc_get_account_endpoint_url('project-create');
     $button_label = __('+ Create Project', 'arsol-pfw');
 } elseif ($can_request) {
     $button_url = wc_get_account_endpoint_url('project-request');
     $button_label = __('+ Request Project', 'arsol-pfw');
-} else {
-    $button_url = '';
-    $button_label = '';
 }
 ?>
 
 <?php if ($button_url && $button_label): ?>
     <div class="arsol-table-header-button">
-        <button href="<?php echo esc_url($button_url); ?>" class="arsol-create-or-request-button button">
+        <a href="<?php echo esc_url($button_url); ?>" class="arsol-create-or-request-button button">
             <?php echo esc_html($button_label); ?>
-        </button>
+        </a>
     </div>
 <?php endif; ?>
 
