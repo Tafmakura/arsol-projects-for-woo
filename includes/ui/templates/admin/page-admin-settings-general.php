82974bb623222033b528e172c5834cc54f24f15a
Taf Makura <tafmakura@gmail.com>
2025-06-04 22:04:22 +0200

    Update page-admin-settings-general.php

--- includes/ui/templates/admin/page-admin-settings-general.php
+++ includes/ui/templates/admin/page-admin-settings-general.php
@@ -30,36 +30,3 @@ $settings = get_option('arsol_projects_settings', array());
         <?php submit_button(); ?>
     </form>
 </div>
-
-<script type="text/javascript">
-jQuery(document).ready(function($) {
-    // Function to toggle conditional fields
-    function toggleConditionalFields() {
-        $('.arsol-conditional-field').each(function() {
-            var $field = $(this);
-            var conditionField = $field.data('condition-field');
-            var conditionValue = $field.data('condition-value');
-            var currentValue = $('#' + conditionField).val();
-            
-            if (currentValue === conditionValue) {
-                $field.addClass('arsol-show-field');
-            } else {
-                $field.removeClass('arsol-show-field');
-            }
-        });
-    }
-    
-    // Initial state - all conditional fields are hidden by CSS
-    toggleConditionalFields();
-    
-    // Listen for changes on all form elements that might be condition fields
-    $('select[id]').on('change', function() {
-        toggleConditionalFields();
-    });
-});
-
-window.arsolConditionalConfig = {
-    field: 'user_project_permissions',
-    value: 'user_specific'
-};
-</script>
