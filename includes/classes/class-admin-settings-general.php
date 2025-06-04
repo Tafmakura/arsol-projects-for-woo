746ad5307aed804a293d46e93370471a0a331275
Taf Makura <tafmakura@gmail.com>
2025-06-04 21:16:48 +0200

    Update class-admin-settings-general.php

--- includes/classes/class-admin-settings-general.php
+++ includes/classes/class-admin-settings-general.php
@@ -40,29 +40,23 @@ class Settings_General {
         // Add settings fields
         add_settings_field(
             'enable_project_comments',
-            '',
+            __('Enable Project Comments', 'arsol-pfw'),
             array($this, 'render_checkbox_field'),
             'arsol_projects_settings',
             'arsol_projects_general_settings',
             array(
-                'label_for' => 'enable_project_comments',
-                'label' => __('Enable Project Comments', 'arsol-pfw'),
-                'description' => __('Allow users to comment on projects', 'arsol-pfw'),
-                'row_class' => 'arsol-settings-row arsol-settings-enable-project-comments'
+                'description' => __('Allow users to comment on projects', 'arsol-pfw')
             )
         );
 
         add_settings_field(
             'projects_per_page',
-            '',
+            __('Projects Per Page', 'arsol-pfw'),
             array($this, 'render_number_field'),
             'arsol_projects_settings',
             'arsol_projects_general_settings',
             array(
-                'label_for' => 'projects_per_page',
-                'label' => __('Projects Per Page', 'arsol-pfw'),
-                'description' => __('Number of projects to display per page', 'arsol-pfw'),
-                'row_class' => 'arsol-settings-row arsol-settings-projects-per-page'
+                'description' => __('Number of projects to display per page', 'arsol-pfw')
             )
         );
 
@@ -76,33 +70,28 @@ class Settings_General {
 
         add_settings_field(
             'user_project_permissions',
-            '',
+            __('Global Permission Setting', 'arsol-pfw'),
             array($this, 'render_select_field'),
             'arsol_projects_settings',
             'arsol_projects_user_permissions',
             array(
-                'label_for' => 'user_project_permissions',
-                'label' => __('Global Permission Setting', 'arsol-pfw'),
                 'description' => __('Controls how user project permissions are handled globally', 'arsol-pfw'),
                 'options' => array(
                     'none' => __('None - No users can request/create projects', 'arsol-pfw'),
                     'request' => __('Users can request projects', 'arsol-pfw'),
                     'create' => __('Users can create projects', 'arsol-pfw'),
                     'user_specific' => __('User Specific - Individual user settings apply', 'arsol-pfw')
-                ),
-                'row_class' => 'arsol-settings-row arsol-settings-user-project-permissions'
+                )
             )
         );
 
         add_settings_field(
             'default_user_permission',
-            '',
+            __('Default User', 'arsol-pfw'),
             array($this, 'render_conditional_select_field'),
             'arsol_projects_settings',
             'arsol_projects_user_permissions',
             array(
-                'label_for' => 'default_user_permission',
-                'label' => __('Default User', 'arsol-pfw'),
                 'description' => __('Default permission level assigned to new users (only applies when "User Specific" is selected above)', 'arsol-pfw'),
                 'condition_field' => 'user_project_permissions',
                 'condition_value' => 'user_specific',
@@ -111,7 +100,9 @@ class Settings_General {
                     'request' => __('Can request projects', 'arsol-pfw'),
                     'create' => __('Can create projects', 'arsol-pfw')
                 ),
-                'row_class' => 'arsol-settings-row arsol-settings-default-user-permission arsol-conditional-field'
+                'class' => 'arsol-conditional-field',
+                'data-condition-field' => 'user_project_permissions',
+                'data-condition-value' => 'user_specific'
             )
         );
     }
@@ -128,24 +119,16 @@ class Settings_General {
      */
     public function render_checkbox_field($args) {
         $settings = get_option('arsol_projects_settings', array());
-        $value = isset($settings[$args['label_for']]) ? $settings[$args['label_for']] : 0;
-        $row_class = isset($args['row_class']) ? esc_attr($args['row_class']) : 'arsol-settings-row';
+        $value = isset($settings['enable_project_comments']) ? $settings['enable_project_comments'] : 0;
         ?>
-        <tr class="<?php echo $row_class; ?>">
-            <th scope="row">
-                <label for="<?php echo esc_attr($args['label_for']); ?>"><?php echo esc_html($args['label']); ?></label>
-            </th>
-            <td>
-                <input type="checkbox" 
-                       id="<?php echo esc_attr($args['label_for']); ?>"
-                       name="arsol_projects_settings[<?php echo esc_attr($args['label_for']); ?>]"
-                       value="1"
-                       <?php checked(1, $value); ?>>
-                <p class="description">
-                    <?php echo esc_html($args['description']); ?>
-                </p>
-            </td>
-        </tr>
+        <input type="checkbox" 
+               id="enable_project_comments"
+               name="arsol_projects_settings[enable_project_comments]"
+               value="1"
+               <?php checked(1, $value); ?>>
+        <p class="description">
+            <?php echo esc_html($args['description']); ?>
+        </p>
         <?php
     }
 
@@ -154,25 +137,17 @@ class Settings_General {
      */
     public function render_number_field($args) {
         $settings = get_option('arsol_projects_settings', array());
-        $value = isset($settings[$args['label_for']]) ? $settings[$args['label_for']] : 10;
-        $row_class = isset($args['row_class']) ? esc_attr($args['row_class']) : 'arsol-settings-row';
+        $value = isset($settings['projects_per_page']) ? $settings['projects_per_page'] : 10;
         ?>
-        <tr class="<?php echo $row_class; ?>">
-            <th scope="row">
-                <label for="<?php echo esc_attr($args['label_for']); ?>"><?php echo esc_html($args['label']); ?></label>
-            </th>
-            <td>
-                <input type="number" 
-                       id="<?php echo esc_attr($args['label_for']); ?>"
-                       name="arsol_projects_settings[<?php echo esc_attr($args['label_for']); ?>]"
-                       value="<?php echo esc_attr($value); ?>"
-                       min="1"
-                       max="100">
-                <p class="description">
-                    <?php echo esc_html($args['description']); ?>
-                </p>
-            </td>
-        </tr>
+        <input type="number" 
+               id="projects_per_page"
+               name="arsol_projects_settings[projects_per_page]"
+               value="<?php echo esc_attr($value); ?>"
+               min="1"
+               max="100">
+        <p class="description">
+            <?php echo esc_html($args['description']); ?>
+        </p>
         <?php
     }
 
@@ -181,27 +156,19 @@ class Settings_General {
      */
     public function render_select_field($args) {
         $settings = get_option('arsol_projects_settings', array());
-        $value = isset($settings[$args['label_for']]) ? $settings[$args['label_for']] : 'none';
-        $row_class = isset($args['row_class']) ? esc_attr($args['row_class']) : 'arsol-settings-row';
+        $value = isset($settings['user_project_permissions']) ? $settings['user_project_permissions'] : 'none';
         ?>
-        <tr class="<?php echo $row_class; ?>">
-            <th scope="row">
-                <label for="<?php echo esc_attr($args['label_for']); ?>"><?php echo esc_html($args['label']); ?></label>
-            </th>
-            <td>
-                <select id="<?php echo esc_attr($args['label_for']); ?>"
-                        name="arsol_projects_settings[<?php echo esc_attr($args['label_for']); ?>]">
-                    <?php foreach ($args['options'] as $option => $label): ?>
-                        <option value="<?php echo esc_attr($option); ?>" <?php selected($value, $option); ?>>
-                            <?php echo esc_html($label); ?>
-                        </option>
-                    <?php endforeach; ?>
-                </select>
-                <p class="description">
-                    <?php echo esc_html($args['description']); ?>
-                </p>
-            </td>
-        </tr>
+        <select id="user_project_permissions"
+                name="arsol_projects_settings[user_project_permissions]">
+            <?php foreach ($args['options'] as $option => $label): ?>
+                <option value="<?php echo esc_attr($option); ?>" <?php selected($value, $option); ?>>
+                    <?php echo esc_html($label); ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+        <p class="description">
+            <?php echo esc_html($args['description']); ?>
+        </p>
         <?php
     }
 
@@ -210,27 +177,19 @@ class Settings_General {
      */
     public function render_conditional_select_field($args) {
         $settings = get_option('arsol_projects_settings', array());
-        $value = isset($settings[$args['label_for']]) ? $settings[$args['label_for']] : 'none';
-        $row_class = isset($args['row_class']) ? esc_attr($args['row_class']) : 'arsol-settings-row';
+        $value = isset($settings['default_user_permission']) ? $settings['default_user_permission'] : 'none';
         ?>
-        <tr class="<?php echo $row_class; ?>" data-condition-field="<?php echo esc_attr($args['condition_field']); ?>" data-condition-value="<?php echo esc_attr($args['condition_value']); ?>">
-            <th scope="row">
-                <label for="<?php echo esc_attr($args['label_for']); ?>"><?php echo esc_html($args['label']); ?></label>
-            </th>
-            <td>
-                <select id="<?php echo esc_attr($args['label_for']); ?>"
-                        name="arsol_projects_settings[<?php echo esc_attr($args['label_for']); ?>]">
-                    <?php foreach ($args['options'] as $option => $label): ?>
-                        <option value="<?php echo esc_attr($option); ?>" <?php selected($value, $option); ?>>
-                            <?php echo esc_html($label); ?>
-                        </option>
-                    <?php endforeach; ?>
-                </select>
-                <p class="description">
-                    <?php echo esc_html($args['description']); ?>
-                </p>
-            </td>
-        </tr>
+        <select id="default_user_permission"
+                name="arsol_projects_settings[default_user_permission]">
+            <?php foreach ($args['options'] as $option => $label): ?>
+                <option value="<?php echo esc_attr($option); ?>" <?php selected($value, $option); ?>>
+                    <?php echo esc_html($label); ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+        <p class="description">
+            <?php echo esc_html($args['description']); ?>
+        </p>
         <?php
     }
 
