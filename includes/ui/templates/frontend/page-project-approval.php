76bfa3510aa19abe10bdd97102bcfc1ee39259ea
Taf Makura <tafmakura@gmail.com>
2025-06-05 21:15:40 +0200

    Update page-project-approval.php

--- includes/ui/templates/frontend/page-project-approval.php
+++ includes/ui/templates/frontend/page-project-approval.php
@@ -6,9 +6,10 @@
  * @since 1.0.0
  */
 
+if (!defined('ABSPATH')) {
+    exit;
+}
 
-
-/* 
 // Get the post type and ID
 $type = isset($_GET['type']) ? sanitize_text_field($_GET['type']) : '';
 $post_id = 0;
@@ -55,7 +56,3 @@ if (file_exists($component_path)) {
     wp_safe_redirect(wc_get_account_endpoint_url('projects'));
     exit;
 }
-*/
-
-?>
-Hello World
