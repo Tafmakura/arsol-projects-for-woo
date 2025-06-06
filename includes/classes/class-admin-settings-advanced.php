64652604beb1fbf1b80accf13f1efd737df582f9
Taf Makura <tafmakura@gmail.com>
2025-06-06 18:08:19 +0200

    Update class-admin-settings-advanced.php

--- includes/classes/class-admin-settings-advanced.php
+++ includes/classes/class-admin-settings-advanced.php
@@ -18,13 +18,34 @@ class Settings_Advanced {
 
     public function __construct() {
         $this->shortcode_fields = [
-            'project_overview_shortcode' => __('Project Overview', 'arsol-pfw'),
-            'create_project_form_shortcode' => __('Create Project Form', 'arsol-pfw'),
-            'request_project_form_shortcode' => __('Request Project Form', 'arsol-pfw'),
-            'projects_listing_shortcode' => __('Projects Listing', 'arsol-pfw'),
-            'project_proposal_listings_shortcode' => __('Project Proposal Listings', 'arsol-pfw'),
-            'project_requests_listings_shortcode' => __('Project Requests Listings', 'arsol-pfw'),
-            'access_denied_shortcode' => __('Access Denied', 'arsol-pfw'),
+            'project_overview_shortcode' => [
+                'title' => __('Project Overview', 'arsol-pfw'),
+                'description' => __('Overrides the single project view.', 'arsol-pfw')
+            ],
+            'create_project_form_shortcode' => [
+                'title' => __('Create Project Form', 'arsol-pfw'),
+                'description' => __('Overrides the form for creating new projects.', 'arsol-pfw')
+            ],
+            'request_project_form_shortcode' => [
+                'title' => __('Request Project Form', 'arsol-pfw'),
+                'description' => __('Overrides the form for requesting new projects.', 'arsol-pfw')
+            ],
+            'projects_listing_shortcode' => [
+                'title' => __('Projects Listing', 'arsol-pfw'),
+                'description' => __('Overrides the display of all projects for a user.', 'arsol-pfw')
+            ],
+            'project_proposal_listings_shortcode' => [
+                'title' => __('Project Proposal Listings', 'arsol-pfw'),
+                'description' => __('Overrides the display of project proposals.', 'arsol-pfw')
+            ],
+            'project_requests_listings_shortcode' => [
+                'title' => __('Project Requests Listings', 'arsol-pfw'),
+                'description' => __('Overrides the display of project requests.', 'arsol-pfw')
+            ],
+            'access_denied_shortcode' => [
+                'title' => __('Access Denied', 'arsol-pfw'),
+                'description' => __('Overrides the page shown when a user has insufficient permissions.', 'arsol-pfw')
+            ],
         ];
         add_action('admin_init', array($this, 'register_settings'));
     }
@@ -39,16 +60,17 @@ class Settings_Advanced {
             'arsol_projects_advanced_settings'
         );
 
-        foreach ($this->shortcode_fields as $id => $title) {
+        foreach ($this->shortcode_fields as $id => $field_data) {
             add_settings_field(
                 $id,
-                $title,
+                $field_data['title'],
                 array($this, 'render_text_field'),
                 'arsol_projects_advanced_settings',
                 'arsol_projects_template_overrides_section',
                 [
                     'id' => $id,
-                    'pattern' => '^\\[[a-zA-Z0-9\\s_-]+\\]$'
+                    'pattern' => '^\\[[a-zA-Z0-9\\s_-]+\\]$',
+                    'description' => $field_data['description']
                 ]
             );
         }
@@ -70,6 +92,9 @@ class Settings_Advanced {
                class="regular-text"
                pattern="<?php echo esc_attr($pattern); ?>"
                title="<?php esc_attr_e('Shortcode must be in the format [shortcode_name]', 'arsol-pfw'); ?>">
+        <?php if (!empty($args['description'])) : ?>
+            <p class="description"><?php echo esc_html($args['description']); ?></p>
+        <?php endif; ?>
         <?php
     }
 }
