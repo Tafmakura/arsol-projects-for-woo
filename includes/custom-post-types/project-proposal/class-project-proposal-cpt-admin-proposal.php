25702ed05299404bfd46cef526c5216d6175b142
Taf Makura <tafmakura@gmail.com>
2025-06-08 12:36:59 +0200

    Update class-project-proposal-cpt-admin-proposal.php

--- includes/custom-post-types/project-proposal/class-project-proposal-cpt-admin-proposal.php
+++ includes/custom-post-types/project-proposal/class-project-proposal-cpt-admin-proposal.php
@@ -109,7 +109,6 @@ class Proposal {
 
             <div id="recurring_billing_cycle_wrapper" style="<?php echo empty($recurring_budget) || $recurring_budget <= 0 ? 'display: none;' : ''; ?>">
                 <p>
-                    <label for="proposal_billing_cycle" style="display:block;margin-bottom:5px;"><?php _e('Recurring Billing Cycle:', 'arsol-pfw'); ?></label>
                     <span style="display: flex; justify-content: space-between;">
                         <select id="proposal_billing_interval" name="proposal_billing_interval" style="width: 48%;">
                             <?php
