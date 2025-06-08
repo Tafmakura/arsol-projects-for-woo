d1fc840f09fca0382661daa23dcb27c72cdb6291
Taf Makura <tafmakura@gmail.com>
2025-06-09 00:29:13 +0200

    Update admin-proposal-invoice.js

--- includes/custom-post-types/project-proposal/assets/admin-proposal-invoice.js
+++ includes/custom-post-types/project-proposal/assets/admin-proposal-invoice.js
@@ -5,8 +5,9 @@
     var ArsolProposalInvoice = {
         init: function() {
             this.product_template = wp.template('arsol-product-line-item');
-            this.fee_template = wp.template('arsol-onetime-fee-line-item');
-            this.line_item_id = 0; // A single counter for all line items to ensure uniqueness
+            this.onetime_fee_template = wp.template('arsol-onetime-fee-line-item');
+            this.recurring_fee_template = wp.template('arsol-recurring-fee-line-item');
+            this.line_item_id = 0;
             this.bindEvents();
             this.loadExistingItems();
         },
@@ -16,35 +17,45 @@
             builder.on('click', '.add-line-item', this.addLineItem.bind(this));
             builder.on('click', '.remove-line-item', this.removeLineItem.bind(this));
             builder.on('change', '.product-select', this.productChanged.bind(this));
-            builder.on('input', '.quantity-input, .sale-price-input, .fee-amount-input', this.calculateTotals.bind(this));
+            builder.on('input', '.quantity-input, .sale-price-input, .price-input, .fee-amount-input', this.calculateTotals.bind(this));
         },
         
         loadExistingItems: function() {
             var self = this;
             var items = arsol_proposal_invoice_vars.line_items;
 
-            // Load products
             if (items && items.products) {
-                $.each(items.products, function(id, itemData) {
-                    self.line_item_id++;
-                    itemData.id = self.line_item_id;
-                    var $newRow = $(self.product_template(itemData));
-                    $('#product-lines-body').append($newRow);
-                    self.initSelect2($newRow);
-                });
+                $.each(items.products, function(id, itemData) { self.renderRow('product', itemData); });
             }
-            
-            // Load fees
             if (items && items.one_time_fees) {
-                 $.each(items.one_time_fees, function(id, itemData) {
-                    self.line_item_id++;
-                    itemData.id = self.line_item_id;
-                    var $newRow = $(self.fee_template(itemData));
-                    $('#onetime-fee-lines-body').append($newRow);
-                });
+                $.each(items.one_time_fees, function(id, itemData) { self.renderRow('onetime-fee', itemData); });
+            }
+            if (items && items.recurring_fees) {
+                $.each(items.recurring_fees, function(id, itemData) { self.renderRow('recurring-fee', itemData); });
             }
 
             this.calculateTotals();
+        },
+
+        renderRow: function(type, data) {
+            this.line_item_id++;
+            data.id = this.line_item_id;
+            var template, container;
+
+            if (type === 'product') {
+                template = this.product_template;
+                container = '#product-lines-body';
+            } else if (type === 'onetime-fee') {
+                template = this.onetime_fee_template;
+                container = '#onetime-fee-lines-body';
+            } else if (type === 'recurring-fee') {
+                template = this.recurring_fee_template;
+                container = '#recurring-fee-lines-body';
+            }
+            
+            var $newRow = $(template(data));
+            $(container).append($newRow);
+            if (type === 'product') this.initSelect2($newRow);
         },
 
         initSelect2: function($row) {
@@ -60,9 +71,7 @@
                             search: params.term,
                         };
                     },
-                    processResults: function(data) {
-                        return { results: data.data };
-                    },
+                    processResults: function(data) { return { results: data.data }; },
                     cache: true
                 },
                 placeholder: 'Search for a product...',
@@ -72,18 +81,7 @@
 
         addLineItem: function(e) {
             e.preventDefault();
-            this.line_item_id++;
-            var templateData = { id: this.line_item_id };
-            var itemType = $(e.currentTarget).data('type');
-
-            if (itemType === 'product') {
-                var $newRow = $(this.product_template(templateData));
-                $('#product-lines-body').append($newRow);
-                this.initSelect2($newRow);
-            } else if (itemType === 'onetime-fee') {
-                var $newRow = $(this.fee_template(templateData));
-                $('#onetime-fee-lines-body').append($newRow);
-            }
+            this.renderRow($(e.currentTarget).data('type'), {});
         },
 
         removeLineItem: function(e) {
@@ -110,8 +108,11 @@
                 },
                 success: function(response) {
                     if (response.success) {
-                        $row.find('.price-input').val(response.data.price);
-                        $row.find('.sale-price-input').val(response.data.sale_price);
+                        var data = response.data;
+                        $row.find('.price-input').val(data.regular_price).prop('readonly', data.is_subscription);
+                        $row.find('.sale-price-input').val(data.sale_price).prop('readonly', data.is_subscription);
+                        $row.find('.product-sub-text').html(data.sub_text);
+                        $row.data('is-subscription', data.is_subscription);
                         self.calculateTotals();
                     }
                 }
@@ -119,42 +120,57 @@
         },
         
         calculateTotals: function() {
-            var grandTotal = 0;
+            var oneTimeTotal = 0;
+            var recurringTotal = 0;
             var currencySymbol = arsol_proposal_invoice_vars.currency_symbol;
+            
+            var formatPrice = function(price) {
+                var formattedPrice = price.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                return '<span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">' + currencySymbol + '</span>' + formattedPrice + '</bdi></span>';
+            };
 
+            // Product Totals
             $('#product-lines-body .line-item').each(function() {
                 var $row = $(this);
                 var quantity = parseFloat($row.find('.quantity-input').val()) || 0;
                 var salePrice = parseFloat($row.find('.sale-price-input').val());
                 var price = parseFloat($row.find('.price-input').val()) || 0;
+                var isSubscription = $row.data('is-subscription');
                 
                 var unitPrice = !isNaN(salePrice) && salePrice > 0 ? salePrice : price;
                 var subtotal = quantity * unitPrice;
-                
-                grandTotal += subtotal;
-                
-                var formattedPrice = subtotal.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                var priceHtml = '<span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">' + currencySymbol + '</span>' + formattedPrice + '</bdi></span>';
 
-                $row.find('.subtotal-display').html(priceHtml);
+                if (isSubscription) {
+                    recurringTotal += subtotal;
+                } else {
+                    oneTimeTotal += subtotal;
+                }
+                
+                $row.find('.subtotal-display').html(formatPrice(subtotal));
             });
-
+            
+            // One-Time Fee Totals
             $('#onetime-fee-lines-body .line-item').each(function() {
                 var $row = $(this);
                 var amount = parseFloat($row.find('.fee-amount-input').val()) || 0;
-                grandTotal += amount;
+                oneTimeTotal += amount;
+                $row.find('.subtotal-display').html(formatPrice(amount));
+            });
 
-                var formattedPrice = amount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                var priceHtml = '<span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">' + currencySymbol + '</span>' + formattedPrice + '</bdi></span>';
-
-                $row.find('.subtotal-display').html(priceHtml);
+            // Recurring Fee Totals
+            $('#recurring-fee-lines-body .line-item').each(function() {
+                var $row = $(this);
+                var amount = parseFloat($row.find('.fee-amount-input').val()) || 0;
+                recurringTotal += amount;
+                var interval = $row.find('.billing-interval option:selected').text();
+                var period = $row.find('.billing-period option:selected').text();
+                $row.find('.subtotal-display').html(formatPrice(amount) + ' / ' + period);
             });
             
-            var formattedGrandPrice = grandTotal.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-            var grandTotalHtml = '<span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">' + currencySymbol + '</span>' + formattedGrandPrice + '</bdi></span>';
-
-            $('#grand-total-display').html(grandTotalHtml);
-            $('#line_items_grand_total').val(grandTotal.toFixed(2));
+            $('#one-time-total-display').html(formatPrice(oneTimeTotal));
+            $('#recurring-total-display').html(formatPrice(recurringTotal));
+            $('#line_items_one_time_total').val(oneTimeTotal.toFixed(2));
+            $('#line_items_recurring_total').val(recurringTotal.toFixed(2));
         }
     };
 
@@ -162,4 +178,4 @@
         ArsolProposalInvoice.init();
     });
 
-})(jQuery); 
+})(jQuery);
