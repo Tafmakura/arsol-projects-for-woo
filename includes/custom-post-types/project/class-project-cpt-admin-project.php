a896f56f654de5d02c202656399963c25547baec
Taf Makura <tafmakura@gmail.com>
2025-06-12 18:32:14 +0200

    Update

--- includes/custom-post-types/project/class-project-cpt-admin-project.php
+++ includes/custom-post-types/project/class-project-cpt-admin-project.php
@@ -32,91 +32,9 @@ class Project {
     public function render_project_details_meta_box($post) {
         // Add nonce for security
         wp_nonce_field('project_details_meta_box', 'project_details_meta_box_nonce');
-
-        // Get current values
-        $current_status_terms = wp_get_object_terms($post->ID, 'arsol-project-status', array('fields' => 'slugs'));
-        $current_status = !empty($current_status_terms) ? $current_status_terms[0] : 'not-started';
-
-        $due_date = get_post_meta($post->ID, '_project_due_date', true);
-        $standard_order_id = get_post_meta($post->ID, '_standard_order_id', true);
-        $recurring_order_id = get_post_meta($post->ID, '_recurring_order_id', true);
-        
-        // Get statuses, excluding 'not-started' if the current status is not 'not-started'
-        $statuses_args = array(
-            'taxonomy'   => 'arsol-project-status',
-            'hide_empty' => false,
-        );
-        if ($current_status !== 'not-started') {
-            $not_started_term = get_term_by('slug', 'not-started', 'arsol-project-status');
-            if ($not_started_term) {
-                $statuses_args['exclude'] = array($not_started_term->term_id);
-            }
-        }
-        $statuses = get_terms($statuses_args);
-
-        // Get author dropdown
-        $author_dropdown = wp_dropdown_users(array(
-            'name' => 'post_author_override',
-            'selected' => $post->post_author,
-            'include_selected' => true,
-            'echo' => false,
-            'class' => 'widefat'
-        ));
         ?>
         <div class="project-details">
-            <p>
-                <label for="project_id" style="display:block;margin-bottom:5px;"><?php _e('Project ID:', 'arsol-pfw'); ?></label>
-                <input type="text" 
-                       id="project_id" 
-                       value="<?php echo esc_attr($post->ID); ?>"
-                       disabled
-                       class="widefat">
-            </p>
-
-            <p>
-                <label for="post_author_override"><?php _e('Customer:', 'arsol-pfw'); ?></label>
-                <?php echo $author_dropdown; ?>
-            </p>
-
-            <p>
-                <label for="project_status"><?php _e('Project Status:', 'arsol-pfw'); ?></label>
-                <select name="project_status" id="project_status" style="width:100%">
-                    <?php foreach ($statuses as $status) : ?>
-                        <option value="<?php echo esc_attr($status->slug); ?>" <?php selected($current_status, $status->slug); ?>>
-                            <?php echo esc_html($status->name); ?>
-                        </option>
-                    <?php endforeach; ?>
-                </select>
-            </p>
-
-            <p>
-                <label for="project_due_date"><?php _e('Due Date:', 'arsol-pfw'); ?></label>
-                <input type="date" 
-                       id="project_due_date" 
-                       name="project_due_date" 
-                       value="<?php echo esc_attr($due_date); ?>"
-                       style="width:100%">
-            </p>
-
-            <?php if ($standard_order_id || $recurring_order_id) : ?>
-                <p>
-                    <label><?php _e('Proposal Invoices:', 'arsol-pfw'); ?></label>
-                    <span class="arsol-pfw-invoice-links">
-                        <?php 
-                        $links = array();
-                        if ($standard_order_id) {
-                            $links[] = '<a href="' . get_edit_post_link($standard_order_id) . '">#' . esc_html($standard_order_id) . '</a>';
-                        }
-                        if ($recurring_order_id) {
-                             $links[] = '<a href="' . get_edit_post_link($recurring_order_id) . '">#' . esc_html($recurring_order_id) . '</a>';
-                        }
-                        echo implode(', ', $links);
-                        ?>
-                    </span>
-                </p>
-            <?php endif; ?>
-            
-            <div class="major-actions" style="margin-top: 20px;">
+            <div class="major-actions">
                 <input type="submit" class="button button-primary" value="<?php echo ($post->post_status === 'publish') ? __('Update', 'arsol-pfw') : __('Publish', 'arsol-pfw'); ?>" style="width: 100%;">
             </div>
         </div>
