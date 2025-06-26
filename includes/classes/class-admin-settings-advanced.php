33aa393e45953387240eaaad0ab3cd11346188e2
Taf Makura <tafmakura@gmail.com>
2025-06-26 23:54:27 +0200

    Update class-admin-settings-advanced.php

--- includes/classes/class-admin-settings-advanced.php
+++ includes/classes/class-admin-settings-advanced.php
@@ -26,6 +26,9 @@ class Settings_Advanced {
         
         // Add AJAX handler for reset defaults
         add_action('wp_ajax_arsol_reset_defaults', array($this, 'handle_reset_defaults_ajax'));
+        
+        // Add admin scripts
+        add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_scripts'));
     }
 
     public function init_translations() {
@@ -301,25 +304,6 @@ class Settings_Advanced {
         <?php if (!empty($args['description'])) : ?>
             <p class="description"><?php echo esc_html($args['description']); ?></p>
         <?php endif; ?>
-        
-        <?php
-        // Add JavaScript for loading defaults (only once)
-        static $script_added = false;
-        if (!$script_added) :
-            $script_added = true;
-            ?>
-            <script type="text/javascript">
-            var arsolDefaultContent = <?php echo json_encode($hardcoded_defaults); ?>;
-            
-            function loadDefaultContent(fieldId) {
-                if (arsolDefaultContent[fieldId]) {
-                    if (confirm('<?php esc_js_e('Load the default content? This will replace any existing content in this field.', 'arsol-pfw'); ?>')) {
-                        document.getElementById(fieldId).value = arsolDefaultContent[fieldId];
-                    }
-                }
-            }
-            </script>
-        <?php endif; ?>
         <?php
     }
 
@@ -413,5 +397,37 @@ class Settings_Advanced {
         });
         </script>
         <?php
+    }
+
+    /**
+     * Enqueue admin scripts
+     */
+    public function enqueue_admin_scripts($hook) {
+        // Only load on our settings page
+        if (strpos($hook, 'arsol-projects-settings') !== false) {
+            // Ensure jQuery is loaded
+            wp_enqueue_script('jquery');
+            
+            // Get hardcoded defaults for JavaScript
+            $hardcoded_defaults = \Arsol_Projects_For_Woo\Admin\Setup_Defaults::get_hardcoded_defaults();
+            
+            // Add inline script with proper escaping
+            $script = '
+                window.arsolDefaultContent = ' . wp_json_encode($hardcoded_defaults) . ';
+                
+                window.loadDefaultContent = function(fieldId) {
+                    if (window.arsolDefaultContent && window.arsolDefaultContent[fieldId]) {
+                        if (confirm(' . wp_json_encode(__('Load the default content? This will replace any existing content in this field.', 'arsol-pfw')) . ')) {
+                            var field = document.getElementById(fieldId);
+                            if (field) {
+                                field.value = window.arsolDefaultContent[fieldId];
+                            }
+                        }
+                    }
+                };
+            ';
+            
+            wp_add_inline_script('jquery', $script);
+        }
     }
 }
