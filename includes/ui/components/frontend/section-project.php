3bf3c290efd6eeaee1959cd5fa35e7954e59e129
Taf Makura <tafmakura@gmail.com>
2025-06-27 04:58:27 +0200

    Update section-project.php

--- includes/ui/components/frontend/section-project.php
+++ includes/ui/components/frontend/section-project.php
@@ -139,9 +139,11 @@ if (\Arsol_Projects_For_Woo\Frontend_Template_Overrides::has_project_overview_ov
             
             <div class="project-sidebar-wrapper">
                 <div class="project-sidebar-card card">
-                    ddfgfgdfgfdfgdfgdfg
+                  
                     <?php
                     if (file_exists($sidebar_template)) {
+
+                        echo 'sdfsdf';
                         include $sidebar_template;
                     } else {
                         echo '<p>' . esc_html__('Sidebar template not found.', 'arsol-pfw') . '</p>';
