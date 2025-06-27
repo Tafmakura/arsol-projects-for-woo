99c677b70920680394eef064dbfa0fc59d566689
Taf Makura <tafmakura@gmail.com>
2025-06-27 03:29:59 +0200

    Update class-project-proposal-cpt-admin-proposal.php

--- includes/custom-post-types/project-proposal/class-project-proposal-cpt-admin-proposal.php
+++ includes/custom-post-types/project-proposal/class-project-proposal-cpt-admin-proposal.php
@@ -52,7 +52,7 @@ class Proposal {
             array($this, 'render_processing_feedback_metabox'),
             'arsol-pfw-proposal',
             'normal',
-            'high',
+            'core',
             array('__back_compat_meta_box' => false, 'class' => 'arsol-pfw-show-if-proposal-status-is-processing')
         );
         
@@ -62,7 +62,7 @@ class Proposal {
             array($this, 'render_pending_approval_feedback_metabox'),
             'arsol-pfw-proposal',
             'normal',
-            'high',
+            'core',
             array('__back_compat_meta_box' => false, 'class' => 'arsol-pfw-show-if-proposal-status-is-pending-approval')
         );
     }
