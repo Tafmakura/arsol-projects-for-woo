78370ae73a6644ba5a1d2804cf05301c4c1f91dc
Taf Makura <tafmakura@gmail.com>
2025-06-27 02:37:09 +0200

    Update page-admin-settings-templates.php

--- includes/ui/templates/admin/page-admin-settings-templates.php
+++ includes/ui/templates/admin/page-admin-settings-templates.php
@@ -15,7 +15,7 @@ if (!defined('ABSPATH')) {
         <?php
         settings_fields('arsol_projects_templates_settings');
         do_settings_sections('arsol_projects_templates_settings');
-        submit_button(__('Save', 'arsol-pfw'));
+        submit_button();
         ?>
     </form>
 </div> 
