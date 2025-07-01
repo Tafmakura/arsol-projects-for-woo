1b3062381079e786f8684d04d088ad05dd6e7515
Taf Makura <tafmakura@gmail.com>
2025-07-02 00:26:17 +0200

    update

--- includes/classes/class-admin-settings-phases.php
+++ includes/classes/class-admin-settings-phases.php
@@ -187,6 +187,9 @@ class Settings_Phases {
     /**
      * Render content display field
      */
+    /**
+     * Render content display field
+     */
     public function render_content_display_field($args) {
         $settings = get_option('arsol_content_display_settings', array());
         $type = $args['type'];
@@ -205,29 +208,26 @@ class Settings_Phases {
             $terms = array();
         }
         
-        echo '<table style="width: 100%;"><tr>';
-        echo '<td style="width: 250px; vertical-align: top;">';
-        
-        // Visibility select
-        echo '<select name="arsol_content_display_settings[' . esc_attr($type) . '_visibility]" style="width: 100%;">';
+        // Visibility select (on top)
+        echo '<div style="margin-bottom: 10px;">';
+        echo '<select name="arsol_content_display_settings[' . esc_attr($type) . '_visibility]" style="width: 250px;">';
         echo '<option value="hide"' . selected($visibility, 'hide', false) . '>' . __('Hide for selected stages', 'arsol-pfw') . '</option>';
         echo '<option value="show"' . selected($visibility, 'show', false) . '>' . __('Show for selected stages', 'arsol-pfw') . '</option>';
         echo '</select>';
+        echo '</div>';
         
-        echo '</td><td style="vertical-align: top;">';
-        
-        // Select2 multi-select for stages
+        // Select2 multi-select for stages (below)
+        echo '<div style="margin-bottom: 10px;">';
         echo '<select name="arsol_content_display_settings[' . esc_attr($type) . '_stages][]" multiple class="arsol-stages-select2" style="width: 100%; min-width: 300px;">';
         foreach ($terms as $term) {
             $selected = in_array($term->term_id, $stages) ? 'selected' : '';
             echo '<option value="' . esc_attr($term->term_id) . '" ' . $selected . '>' . esc_html($term->name) . '</option>';
         }
         echo '</select>';
+        echo '</div>';
         
-        echo '</td></tr></table>';
         echo '<p class="description">' . sprintf(__('Control when %s content appears on the frontend based on the current stage.', 'arsol-pfw'), esc_html($type)) . '</p>';
     }
-
     /**
      * Enqueue admin scripts for Select2
      */
