1c0c84cdade42c2ac1592853195f2e2885f68b96
Taf Makura <tafmakura@gmail.com>
2025-07-09 05:17:27 +0200

    Update class-arsol-pfw-taxonomies-setup.php

--- includes/taxonomies/class-arsol-pfw-taxonomies-setup.php
+++ includes/taxonomies/class-arsol-pfw-taxonomies-setup.php
@@ -78,36 +78,30 @@ class Taxonomies_Setup {
      */
     public function reorder_taxonomy_menus() {
         global $menu;
-        
+
         if (!is_admin() || !current_user_can('manage_options')) {
             return;
         }
-    }
-        
+
         // Find the positions of our stage taxonomy menus
         $stage_positions = array();
-        
+
         if (is_array($menu)) {
-        foreach ($menu as $position => $menu_item) {
-            if (isset($menu_item[2])) {
-                $menu_slug = $menu_item[2];
-                
-                if ($menu_slug === 'edit-tags.php?taxonomy=arsol-pfw-request-stage') {
-                    $stage_positions['request'] = $position;
+            foreach ($menu as $position => $menu_item) {
+                if (isset($menu_item[2])) {
+                    $menu_slug = $menu_item[2];
+
+                    if ($menu_slug === 'edit-tags.php?taxonomy=arsol-pfw-request-stage') {
+                        $stage_positions['request'] = $position;
+                    } elseif ($menu_slug === 'edit-tags.php?taxonomy=arsol-pfw-proposal-stage') {
+                        $stage_positions['proposal'] = $position;
+                    } elseif ($menu_slug === 'edit-tags.php?taxonomy=arsol-pfw-project-stage') {
+                        $stage_positions['project'] = $position;
+                    }
                 }
-    } elseif ($menu_slug === 'edit-tags.php?taxonomy=arsol-pfw-proposal-stage') {
-                    $stage_positions['proposal'] = $position;
-                }
-    } elseif ($menu_slug === 'edit-tags.php?taxonomy=arsol-pfw-project-stage') {
-                    $stage_positions['project'] = $position;
-                }
-    }
             }
-    }
         }
-    }
-        
-        // If we found the menus, they should automatically appear in WordPress admin
+
         // The ordering will be based on registration order, which is already correct
         // in our taxonomies setup class (request -> proposal -> project)
     }
