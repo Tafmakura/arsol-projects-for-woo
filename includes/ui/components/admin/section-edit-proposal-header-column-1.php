dfa9cd4c56f31e1a06ee9433859262a6df8b9d21
Taf Makura <tafmakura@gmail.com>
2025-07-14 00:21:50 +0200

    Update section-edit-proposal-header-column-1.php

--- includes/ui/components/admin/section-edit-proposal-header-column-1.php
+++ includes/ui/components/admin/section-edit-proposal-header-column-1.php
@@ -24,7 +24,7 @@ $proposal_status = get_post_status($post);
 $proposal_stage = $proposal->get_stage();
 $proposal_project_lead = $proposal->get_project_lead();
 $start_date = $proposal->get_start_date();
-$delivery_date = $proposal->get_delivery_date();
+$delivery_date = $proposal->get_due_date();
 $expiration_date = $proposal->get_expiration_date();
 $cost_proposal_type = $proposal->get_costing_type();
 
