d869569d3e9052d68928c98db4902a95c74760ef
Taf Makura <tafmakura@gmail.com>
2025-07-17 23:34:54 +0200

    update

--- ui/templates/frontend/woocommerce/myaccount/no-access.php
+++ ui/templates/frontend/woocommerce/myaccount/no-access.php
@@ -3,6 +3,7 @@
  * No Access template
  * 
  * Shows when user has no access to any projects-related content
+ * This template replaces the entire content wrapper structure
  *
  * @package Arsol_Projects_For_Woo
  * @version 1.1.0
@@ -12,10 +13,89 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-// Check for shortcode override using the new system
-$override = \Arsol_Projects_For_Woo\Frontend\Template\Overrides::get_shortcode_override('[arsol_pfw_no_access]');
-if ($override) {
-    echo do_shortcode($override);
-} else {
-    echo do_shortcode('[arsol_pfw_no_access]');
-} 
+// Set project type for hook compatibility
+$project_type = 'no-access';
+
+// Prepare wrapper data for hooks
+$wrapper_data = array('no_access' => true);
+
+/**
+ * Hook: arsol_pfw_project_wrapper_before
+ * 
+ * @param string $project_type Project type: 'no-access'
+ * @param array $data Wrapper data
+ */
+do_action('arsol_pfw_project_wrapper_before', $project_type, $wrapper_data);
+?>
+
+<div class="arsol-pfw-no-access">
+    <div class="arsol-pfw-header">
+        <h3 class="arsol-pfw-title"><?php esc_html_e('Access Denied', 'arsol-pfw'); ?></h3>
+    </div>
+    
+    <div class="arsol-pfw-content-wrapper" id="no-access-wrapper">
+        <?php
+        /**
+         * Hook: arsol_pfw_project_wrapper_start
+         * 
+         * @param string $project_type Project type: 'no-access'
+         * @param array $data Wrapper data
+         */
+        do_action('arsol_pfw_project_wrapper_start', $project_type, $wrapper_data);
+        ?>
+        
+        <div class="arsol-pfw-content">
+            <?php
+            /**
+             * Hook: arsol_pfw_project_content_before
+             * 
+             * @param string $project_type Project type: 'no-access'
+             * @param array $data Wrapper data
+             */
+            do_action('arsol_pfw_project_content_before', $project_type, $wrapper_data);
+            ?>
+            
+            <div class="arsol-no-permission">
+                <?php
+                // Check for shortcode override using the new system
+                $override = \Arsol_Projects_For_Woo\Frontend\Template\Overrides::get_shortcode_override('[arsol_pfw_no_access]');
+                if ($override) {
+                    echo do_shortcode($override);
+                } else {
+                    echo do_shortcode('[arsol_pfw_no_access]');
+                }
+                ?>
+            </div>
+            
+            <?php
+            /**
+             * Hook: arsol_pfw_project_content_after
+             * 
+             * @param string $project_type Project type: 'no-access'
+             * @param array $data Wrapper data
+             */
+            do_action('arsol_pfw_project_content_after', $project_type, $wrapper_data);
+            ?>
+        </div>
+        
+        <?php
+        /**
+         * Hook: arsol_pfw_project_wrapper_end
+         * 
+         * @param string $project_type Project type: 'no-access'
+         * @param array $data Wrapper data
+         */
+        do_action('arsol_pfw_project_wrapper_end', $project_type, $wrapper_data);
+        ?>
+    </div>
+</div>
+
+<?php
+/**
+ * Hook: arsol_pfw_project_wrapper_after
+ * 
+ * @param string $project_type Project type: 'no-access'
+ * @param array $data Wrapper data
+ */
+do_action('arsol_pfw_project_wrapper_after', $project_type, $wrapper_data);
+?>
